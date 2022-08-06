f547ad46aebd8bf6d2d94606c82aabc6ec7d2085
Anna Navoznova <anna.navoznova@gmail.com>
2022-08-06 19:55:15 +0800

    nullable expense description

--- Models/Expense.cs
+++ Models/Expense.cs
@@ -3,6 +3,6 @@ using System;
 public class Expense
 {
     public decimal Money { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public DateTime MyProperty { get; set; } = DateTime.Now.Date;
 }
