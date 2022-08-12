cb890c13f2c0997294d98fa55a13804743e60248
Ivan Navoznov <navoznov@users.noreply.github.com>
2022-08-12 13:33:29 +0300

    Added date parsing (#12)

--- Models/Expense.cs
+++ Models/Expense.cs
@@ -7,7 +7,7 @@ namespace ExpensesTelegramBot.Models
     {
         [Index(0)]
         [Format("yyyy-MM-dd")]
-        public DateTime Date { get; set; } = DateTime.Now.Date;
+        public DateTime Date { get; set; }
     
         [Index(1)]
         public decimal Money { get; set; }
@@ -15,8 +15,9 @@ namespace ExpensesTelegramBot.Models
         [Index(2)]
         public string? Description { get; set; }
 
-        public Expense(decimal money, string? description)
+        public Expense(decimal money, DateTime? date, string? description)
         {
+            Date = date ?? DateTime.Now.Date;
             Money = money;
             Description = description;
         }
