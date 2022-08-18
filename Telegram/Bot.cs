55a324379ad61c3793ce884300b8f84ab9de2d0d
Ivan Navoznov <i.navoznov@dodopizza.com>
2022-08-19 07:49:28 +0800

    Removed the unused code

--- Telegram/Bot.cs
+++ Telegram/Bot.cs
@@ -1,15 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using ExpensesTelegramBot.Models;
 using ExpensesTelegramBot.Repositories;
 using ExpensesTelegramBot.Services;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
-using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 namespace ExpensesTelegramBot.Telegram
@@ -17,14 +13,12 @@ namespace ExpensesTelegramBot.Telegram
     public class Bot
     {
         readonly TelegramBotClient _botClient;
-        private readonly IExpensesRepository _expensesRepository;
         private readonly IUpdateHandler _updateHandler;
 
         public Bot(string token)
         {
             _botClient = new TelegramBotClient(token);
             _updateHandler = new UpdateHandler(new CsvExpensesRepository(), new ExpenseParser(), new ExpensePrinter());
-            _expensesRepository = new CsvExpensesRepository();
         }
 
         public async Task Run()
@@ -64,37 +58,6 @@ namespace ExpensesTelegramBot.Telegram
                 Console.WriteLine(errorMessage);
                 return Task.CompletedTask;
             }
-        }
-
-        private async Task SendAllExpensesByCurrentMonth(ITelegramBotClient botClient, long chatId,
-            CancellationToken cancellationToken)
-        {
-            var now = DateTime.Now;
-            var expenses = _expensesRepository.GetAll(now.Year, now.Month);
-            var text = GetExpensesText(expenses);
-
-            await botClient.SendTextMessageAsync(chatId, text: text, cancellationToken: cancellationToken);
-        }
-
-        private static string GetExpensesText(IReadOnlyCollection<Expense> expenses)
-        {
-            if (expenses.Count == 0)
-            {
-                return "No records";
-            }
-
-            var stringBuilder = new StringBuilder();
-            foreach (var expense in expenses)
-            {
-                stringBuilder.Append(expense.Date.ToString("yyyy-MM-dd"));
-                stringBuilder.Append('\t');
-                stringBuilder.Append(expense.Money);
-                stringBuilder.Append('\t');
-                stringBuilder.Append(expense.Description);
-                stringBuilder.AppendLine();
-            }
-
-            return stringBuilder.ToString();
         }
     }
 }
--- Telegram/UpdateHandler.cs
+++ Telegram/UpdateHandler.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using ExpensesTelegramBot.Models;
 using ExpensesTelegramBot.Repositories;
 using ExpensesTelegramBot.Services;
 using Telegram.Bot;
