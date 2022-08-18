89a4af897bc1c1eb410fc6fce63fa24abc89fb51
Ivan Navoznov <navoznov@users.noreply.github.com>
2022-08-18 09:54:20 +0300

    Added DI (#27)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using ExpensesTelegramBot.Telegram;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ExpensesTelegramBot
 {
@@ -9,7 +10,10 @@ namespace ExpensesTelegramBot
 
         static async Task Main(string[] args)
         {
-            var bot = new Bot(token);
+            var serviceProvider =  new ServiceCollection()
+                .AddSingleton(provider => ActivatorUtilities.CreateInstance<Bot>(provider, token))
+                .BuildServiceProvider();
+            var bot = serviceProvider.GetService<Bot>();
             await bot.Run();            
         }
     }
