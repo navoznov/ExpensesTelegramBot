ee418665cdabccaae849e3438f256b0245844037
Ivan Navoznov <i.navoznov@dodopizza.com>
2022-08-19 08:03:30 +0800

    DI usage

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System.Threading.Tasks;
+using ExpensesTelegramBot.Repositories;
+using ExpensesTelegramBot.Services;
 using ExpensesTelegramBot.Telegram;
 using Microsoft.Extensions.DependencyInjection;
+using Telegram.Bot.Exceptions;
 
 namespace ExpensesTelegramBot
 {
@@ -10,11 +13,22 @@ namespace ExpensesTelegramBot
 
         static async Task Main(string[] args)
         {
-            var serviceProvider =  new ServiceCollection()
-                .AddSingleton(provider => ActivatorUtilities.CreateInstance<Bot>(provider, token))
-                .BuildServiceProvider();
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var serviceProvider =  serviceCollection.BuildServiceProvider();
             var bot = serviceProvider.GetService<Bot>();
             await bot.Run();            
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection
+                .AddSingleton(provider => ActivatorUtilities.CreateInstance<Bot>(provider, token))
+                .AddScoped<IExpensesRepository, CsvExpensesRepository>()
+                .AddSingleton<IExpenseParser, ExpenseParser>()
+                .AddSingleton<IExpensePrinter, ExpensePrinter>()
+                .AddScoped<IUpdateHandler, UpdateHandler>()
+                ;
         }
     }
 }
